dba16c8fa1486bc4f6d3476f73e7c397ba1b1bbd
2V_Rufin M. <rufintovi11@gmail.com>
2026-05-02 13:27:20 +0800

    edits

---
+++ TUPEnrolment/TUPV_Enrollment/src/Components/Fillupform.jsx
@@ -0,0 +1,12 @@
+import './fillupform.css'
+function Fillupform() {
+  return (
+    <div className="fillupform-container" style={{ backgroundColor: "#f2f2f2", padding: "20px", borderRadius: "5px" }}>
+        <div className='status'>
+            <h2>Enrollment Status: <span className='status-text'>Open</span></h2>
+        </div>
+    </div>
+  )
+}
+
+export default Fillupform
--- TUPEnrolment/TUPV_Enrollment/src/main.jsx
+++ TUPEnrolment/TUPV_Enrollment/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import layout from './layout.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <layout />
+    <App />
   </StrictMode>,
 )
