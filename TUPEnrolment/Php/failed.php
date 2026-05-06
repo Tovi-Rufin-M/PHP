eb4995fbbd34cb4b814164f972afa240c2057441
2V_Rufin M. <rufintovi11@gmail.com>
2026-05-06 12:46:07 +0800

    edits

--- TUPEnrolment/Php/failed.php
+++ TUPEnrolment/Php/failed.php
@@ -7,7 +7,11 @@
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $pdo->query("SELECT name, units, room, instructor, grade FROM failed_subjects");
+    $stmt = $pdo->query("
+        SELECT fs.name, c.course_name, fs.units, fs.room, fs.instructor, fs.grade
+        FROM failed_subjects fs
+        JOIN courses c ON fs.course_id = c.id
+    ");
     $subjecttoRetake = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <div class="Failed-subjects-card">
@@ -31,7 +35,10 @@
             <?php foreach ($subjecttoRetake as $i => $s) { ?>
                 <tr id="retake-row-<?php echo $i; ?>">
                     <td><input type="checkbox" name="select" class="retake-check"></td>
-                    <td><?php echo htmlspecialchars($s['name']); ?></td>
+                    <td>
+                        <?php echo htmlspecialchars($s['name']); ?>
+                        <span><?php echo htmlspecialchars($s['course_name']); ?></span>
+                    </td>
                     <td><?php echo $s['units']; ?></td>
                     <td><?php echo htmlspecialchars($s['room']); ?></td>
                     <td><?php echo htmlspecialchars($s['instructor']); ?></td>
